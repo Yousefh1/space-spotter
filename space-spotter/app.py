39ec1d761946086d562eb3d304cead601bf90a7d
Your Name <yourname@example.com>
2023-11-03 22:50:55 -0400

    main page done

--- space-spotter/app.py
+++ space-spotter/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title='Home')
 
+@app.route('/parking-grid')
+def parking_grid():
+    return render_template('parking-grid.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
