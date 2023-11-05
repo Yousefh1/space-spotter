d1f8aa2d83b2028beb2c2a671da11f1e26e71d1f
Your Name <yourname@example.com>
2023-11-04 23:54:28 -0400

    first=part-done

--- space-spotter/app.py
+++ space-spotter/app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+from models import Space, Status, Position
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', )
 
 @app.route('/parking-grid')
 def parking_grid():
     return render_template('parking-grid.html')
+
+    elements = request.args.get('one', 'two', 'three', 'four', 'five', 'six')
+
+
 
 
 
